f017d94d102ec016d2046a9bbdea6ec10b35afc3
mohaimen.khan@mail.utoronto.ca <mohaimen.khan@mail.utoronto.ca>
2020-04-09 13:47:45 -0400

    Moved img under static

--- src/Components/MainScreen.js
+++ src/Components/MainScreen.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Typical from 'react-typical';
 import Button from "@material-ui/core/Button";
-import Eggplant from './eggplant.png';
+import Eggplant from '../../public/eggplant.png';
 import Particles from 'react-particles-js';
 import ImgDetection from "./ImgDetection";
 
